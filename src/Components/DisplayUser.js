bbe9777fec75f8a463367906d2e6ec088714236f
Andrew Smith <topcodeandrew@gmail.com>
2020-12-29 13:40:42 -0700

    last commit

--- src/Components/DisplayUser.js
+++ src/Components/DisplayUser.js
@@ -13,8 +13,44 @@ export default class DisplayUser extends Component {
         return (
             <div className="user-card">
                 <header className="placement-indicator">{this.props.index + 1}/{this.props.data.length}</header>
-                <div>USER INFO HERE</div>
+
+
+                <div className="user-info">
+                    {/* NAME */}
+                    <h2 className="name">
+                        {this.props.data[this.props.index].name.first} {this.props.data[this.props.index].name.last}
+                    </h2>
+
+                    <div className="top-three-categories">
+                        <p className='bold'>From:</p>
+                        <p>{this.props.data[this.props.index].city}, {this.props.data[this.props.index].country}</p>
+                    </div>
+
+                    <div className="top-three-categories">
+                        <p className='bold'>Job Title:</p>
+                        <p>{this.props.data[this.props.index].title}</p>
+                    </div>
+
+                    <div className="top-three-categories">
+                        <p className='bold'>Employer:</p>
+
+                        <p>{this.props.data[this.props.index].employer}</p>
+                    </div>
+
+                    <div className='fav-movies'>
+                        <p className='bold'> Favorite Movies: </p>
+                        <ol className="list">
+                            <li>{this.props.data[this.props.index].favoriteMovies[0]}</li>
+                            <li>{this.props.data[this.props.index].favoriteMovies[1]}</li>
+                            <li>{this.props.data[this.props.index].favoriteMovies[2]}</li>
+                        </ol>
+                    </div>
+
+                </div>
             </div>
         )
     }
 }
+
+
+// {this.props.data[this.props.index].city}
--- src/Components/Selector.js
+++ src/Components/Selector.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react'
 
 class Selector extends Component {
+
     constructor() {
         super();
 
-        this.state = {
-            index: 0
-        }
+        // this.state = {
+        //     index: 0
+        // }
     }
 
 
     render() {
+        let next = "Next >"
+        let previous = "< Previous"
         return (
             <div className="selector">
-                <div className="button" onClick={this.props.handlePreviousButton}>Previous</div>
+                <div className="button" onClick={this.props.handlePreviousButton}>{previous}</div>
                 <div className="blue-button-container">
                     <div className="blue-button">Edit</div>
                     <div className="blue-button">Delete</div>
                     <div className="blue-button">New</div>
                 </div>
-                <div className="button" onClick={this.props.handleNextButton}>Next</div>
+                <div className="button" onClick={this.props.handleNextButton}>{next}</div>
             </div >
         )
     }
